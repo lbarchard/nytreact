0770fa46f12fa439c34d517dab8cc83e7acc64af
Les Barchard <lbarchard@Less-MacBook-Pro.local>
2017-06-07 23:19:58 -0400

    added grandchildren

--- app/components/Query.js
+++ app/components/Query.js
@@ -2,12 +2,8 @@
 var React = require("react");
 var ReactDOM = require("react-dom")
 
-// Here we include all of the sub-components
-var Query = require("./Query");
-var Results = require("./Results");
-
 // This is the main component
-var Search = React.createClass({
+var Query = React.createClass({
 
   // Here we set a generic state associated with the number of clicks
 	getInitialState: function() {
@@ -17,15 +13,10 @@ var Search = React.createClass({
 	// Here we describe our component's render method
 	render: function() {
 		return (
-			<div>Hello I am Search
-				<div id="query">
-					<Query/>
-					<Results/>
-				</div>
-			</div>
+			<div>Hello I am Query</div>
 		);
 	}
 });
 
 // Export the component back for use in other files
-module.exports = Search;
+module.exports = Query;
